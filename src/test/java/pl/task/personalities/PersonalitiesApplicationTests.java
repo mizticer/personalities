67cf1833cd80dd81634e503ea1fc18ff86e2063f
Sylwester Wojcik <sylwek3455@gmail.com>
2023-11-20 17:47:22 +0100

    init project

---
+++ src/test/java/pl/task/personalities/PersonalitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.task.personalities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
